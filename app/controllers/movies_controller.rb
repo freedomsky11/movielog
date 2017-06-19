c8dcc8d89c32cd1f757c5260d6bedfc65e3f918c
freedomsky <hanwei_1989@126.com>
2017-06-19 16:05:55 +0800

    check owner permission when access edit/update/desroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
 
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @movies = Movie.all
@@ -16,12 +16,16 @@ class MoviesController < ApplicationController
 
   def edit
     @movie = Movie.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 
   def create
     @movie = Movie.new(movie_params)
     @movie.user = current_user
-    
+
     if @movie.save
       redirect_to movies_path
     else
@@ -31,6 +35,11 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
     if @movie.update(movie_params)
       redirect_to movies_path, notice: "Update success."
     else
@@ -40,6 +49,11 @@ class MoviesController < ApplicationController
 
   def destroy
     @movie = Movie.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+    
     @movie.destroy
     flash[alert] = "Movie deleted."
     redirect_to movies_path
